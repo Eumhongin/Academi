402f7d83c46d917a03cc39ca55eb91eae3634ed0
Eumhongin <zizon0wpxm@naver.com>
2018-05-13 23:53:12 +0900

    ADMIN 메인페이지 만드는중 php구문 잠시 잠궈놨음. aca_admin.php연결이 필요하면 주석풀고 rename으로 뒤에 확장자 php변경하면 됨.

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "ddsdsdsdd";
+    echo "줄바꾸기<br/>\n";
+    echo "dddd";?>
+
+  </body>
+</html>
