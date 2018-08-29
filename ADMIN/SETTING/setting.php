39a8c603e1f56ef7e4c7cb57711a2b3d8fa283fa
Eumhongin <zizon0wpxm@naver.com>
2018-08-30 01:05:58 +0900

    wdwd

--- ADMIN/SETTING/setting.php
+++ ADMIN/SETTING/setting.php
@@ -10,5 +10,9 @@
     <button onclick="location.href = 'BG/bg_change.php'">배경 수정</button>
     <button onclick="location.href = 'INFO_STUDENT/stu_info_change.php'">학생 정보</button>
     <button onclick="location.href = 'INFO_TEACHER/tea_info_change.php'">교사 정보</button>
+
+    <input type="file" id="selectedFile" style="display: none;" />
+    <button class='picedit' type="button" name="button" onclick="document.getElementById('selectedFile').click();">사진추가하기</button>
+
   </body>
 </html>
