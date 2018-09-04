e8a7fc08b1b4d77f36e72e7dc7d270c315e88e6e
Eumhongin <zizon0wpxm@naver.com>
2018-09-04 18:59:31 +0900

    Adding Page modify

--- ADMIN/js/add.js
+++ ADMIN/js/add.js
@@ -1,2 +1,5 @@
 console.log($(window).outerHeight());
-$('.v-line').css({'height':$(window).outerHeight()*0.9+'px'});
+$('.v-line').css({'height':$(window).outerHeight()*0.9+'px','top':$(window).outerHeight()*0.05+'px'});
+$('.ADD_RIGHT_IMG').css({'height':$(window).outerHeight()*0.8+'px','background-image':'url("../img/BG.jpg")'});
+$('.ADD_RIGHT_IMG').css({'margin-top':$(window).outerHeight()*0.1+'px'});
+$('.Preview').css({'right':($(window).outerWidth()*0.5-$('.ADD_RIGHT_IMG').outerWidth())*0.5+'px'})
