f3e6087e38e4e837c87b7b253c3f29c1630e9c9e
Eumhongin <zizon0wpxm@naver.com>
2018-09-05 16:47:42 +0900

    ADD PAGE 홈가는거 빼고 끝

--- ADMIN/js/add.js
+++ ADMIN/js/add.js
@@ -1,5 +1,19 @@
-console.log($(window).outerHeight());
+
 $('.v-line').css({'height':$(window).outerHeight()*0.9+'px','top':$(window).outerHeight()*0.05+'px'});
-$('.ADD_RIGHT_IMG').css({'height':$(window).outerHeight()*0.8+'px','background-image':'url("../img/BG.jpg")'});
+$('.ADD_RIGHT_IMG').css({'height':$(window).outerHeight()*0.8+'px','background-image':'url("../img/Preview.png")'});
 $('.ADD_RIGHT_IMG').css({'margin-top':$(window).outerHeight()*0.1+'px'});
 $('.Preview').css({'right':($(window).outerWidth()*0.5-$('.ADD_RIGHT_IMG').outerWidth())*0.5+'px'})
+
+$('.ADD_LEFT_FORM').css({'height':$(window).outerHeight()*0.8+'px','margin-top':$(window).outerHeight()*0.1+'px'});
+
+//extract file name in file path
+$('#selectedFile').on('change',function (){
+  $.each($('#selectedFile').prop("files"), function(k,v){
+    var filename = v['name'];
+    $('.ALFSFS_FilePath').text(filename);
+    // filename = "blahblah.jpg", without path
+  });
+});
+console.log($('.ALFSFS_FilePath').outerHeight());
+$('.picedit').css({'height':$('.ALFSFS_FilePath').outerHeight()});
+$('.ALFS_Submit').css({});
