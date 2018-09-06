ef7b735a79f01b08981b03d7b7b9b12ca62b3de2
LimJoongSub <quhb2455@gmail.com>
2018-09-07 01:16:17 +0900

    2018-09-07
    1. 문제 추가 기능
    2. DB에 사고력같은거 넣을 컬럼 만듬
    3. 문제 추가시 모든 입력란 필수 입력으로 만듬. but 마지막에 유형 체크박스는 할줄 몰라서 못함.
    4. select box는 php 반복문으로 변경.
    5. 더이상 php를 html로 확장자 변경금지

---
+++ ADMIN/ADDING/add.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<?php
+  // session_start();
+  include("..\..\module\dbConnect.php");
+  include("..\..\module\dbContentsEcho.php");
+
+ ?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>문제추가하기</title>
+    <script src="../js/jquery-min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="../fonts/font.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/add.css">
+    <script type="text/javascript">
+    $(document).ready(function(){
+
+
+    });
+
+    </script>
+
+  </head>
+  <body>
+    <div class="v-line">
+
+    </div>
+    <span class="Preview">미리보기</span>
+    <span class="Subtitle_ADD">문제추가</span>
+    <section class="row">
+      <div class="ADD_LEFT">
+
+          <form  class="ADD_LEFT_FORM" name = "form" action="question_add.php" method="post" enctype="multipart/form-data">
+            <div class="ALF_Shield">
+              <div class="ALFS_File_Shield row">
+                <span class="ALFSFS_FilePath"></span>
+                <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+                <input type="file" name="upload" id="selectedFile" style="display: none;" required/>
+                <button class='picedit' type="button" name="button" onclick="document.getElementById('selectedFile').click();">찾기</button>
+              </div>
+              <br>
+              <div class="Not_Defined">
+                <input type="text" name="" value="" placeholder="뭐들어가기로 했지..">
+              </div>
+              <br>
+              <div class="ALFS_Paging_Number row">
+                <div class="ALFSPN_Page">
+                  <input type="text" name="page" value="" placeholder="쪽수" required>
+                </div>
+                <div class="ALFSPN_Number">
+                  <input type="text" name="num" value="" placeholder="번호" required>
+                </div>
+              </div>
+              <br>
+              <div class="ALFS_Option row">
+                <?php
+                  // 문제지 이름
+                  $book_name = DB_echo("SELECT book_name FROM question_book_name","book_name");
+
+                  //문제 유형
+                  //수 1, 수 2 이런식으로도 선택할 수 있게 해야함
+
+                  //문제 세부 유형
+                  $type_index_name = DB_echo("SELECT type_index_name FROM question_type_math_index", "type_index_name");
+                ?>
+                <select class="ALFSO_Subject" name="subject" required>
+                  <option value="" selected>과목</option>
+                  <option value="수학" >수학</option>
+
+                </select>
+                <select class="ALFSO_Grade" name="grade" required>
+                  <option value="" selected>학년</option>
+                  <option value="1" >1</option>
+                  <option value="2" >2</option>
+                  <option value="3" >3</option>
+                </select>
+                <select class="ALFSO_Bookname" name="book_name" required>
+                  <option value="" selected>책이름</option>
+                  <?php
+                  for($idx = 1; $idx <= count($book_name); $idx++)
+                  {
+                    echo "<option value='".$book_name[$idx-1]."'>".$book_name[$idx-1]."</option>";
+                  }
+                  ?>
+                  <!-- <option value="0" >1</option>
+                  <option value="0" >2</option>
+                  <option value="0" >3</option>
+                  <option value="0" >4</option>
+                  <option value="0" >5</option> -->
+                </select>
+                <select class="ALFSO_type" name="question_type" required>
+                  <option value="" selected>유형</option>
+                  <?php
+                  for($idx = 1; $idx <= count($type_index_name); $idx++)
+                  {
+                    echo "<option value='".$type_index_name[$idx-1]."'>".$type_index_name[$idx-1]."</option>";
+                  }
+                  ?>
+                  <!-- <option value="0" >1</option>
+                  <option value="0" >2</option>
+                  <option value="0" >3</option>
+                  <option value="0" >4</option>
+                  <option value="0" >5</option> -->
+                </select>
+              </div>
+              <br>
+              <div class="ALFS_Difficult">
+                <input type="text" name="difficult" value="" placeholder="난이도 [0 - 10]" required>
+              </div>
+              <br>
+              <div class="ALFS_CheckBox row">
+                <!-- 최소 1개 이상 선택하도록 해야함. -->
+                <li><input type="checkbox" name="type[]" value="사고력"><span>사고력</span></li>
+                <li><input type="checkbox" name="type[]" value="이해력"><span>이해력</span></li>
+                <li><input type="checkbox" name="type[]" value="문제해결력"><span>문제해결력</span></li>
+                <li><input type="checkbox" name="type[]" value="창의력"><span>창의력</span></li>
+              </div>
+              <br>
+
+
+
+
+
+            <button class="ALFS_Submit" type="submit">추가</button>
+          </div>
+        </form>
+
+
+        <br><br>
+
+
+      </div>
+      <div class="ADD_RIGHT">
+        <div class="ADD_RIGHT_IMG">
+
+        </div>
+      </div>
+    </section>
+
+
+
+
+    <!-- <script type="text/javascript">
+      function getSelectValue(frm) // 즐겨찾기에 추가해놈 , 웹마당
+      {
+        frm.textValue.value = frm.book_name.options[frm.book_name.selectedIndex].text;
+        frm.optionValue.value = frm.book_name.options[frm.book_name.selectedIndex].value;
+      }
+
+    </script> -->
+    <script src="../js/add.js" charset="utf-8"></script>
+  </body>
+</html>
--- ADMIN/ADDING/question_add.php
+++ ADMIN/ADDING/question_add.php
@@ -1,11 +1,18 @@
 <?php
+// session_start();
+include("..\..\module\dbConnect.php");
+include("..\..\module\dbContentsEcho.php");
+
 if($_FILES['upload'] != null)
 {
-  $uploaddir = ''.$_SERVER["DOCUMENT_ROOT"].'\academi\Academi\question_add\\';
-  $_FILES['upload']['name'] = "test1.png";
-
+  $uploaddir = ''.$_SERVER["DOCUMENT_ROOT"].'\academi\Academi\ADMIN\ADDING\img\\';
+  $_FILES['upload']['name'] = "".$_POST['grade']."_".$_POST['subject']."_".$_POST['book_name']."_".$_POST['question_type']."_".$_POST['page']."_".$_POST['num'].".png";
+  //학년_과목_문제지이름_유형_쪽수_문제번호.jpg
+  $file_name = "".$_POST['grade']."_".$_POST['subject']."_".$_POST['book_name']."_".$_POST['question_type']."_".$_POST['page']."_".$_POST['num'].".png";
   $uploadfile = $uploaddir . basename($_FILES['upload']['name']);
 
+  $sql = "SELECT type_index_num FROM question_type_math_index WHERE type_index_name = '".$_POST['question_type']."' ";
+  $type_index_num = DB_echo($sql , "type_index_num");
   // $img_sql = "UPDATE userinfo SET AdviserImage = '".$_FILES['upload']['name']."' WHERE useruid=".$_SESSION['Uid']."";
   // mysqli_query($conn,$img_sql);
 
@@ -16,13 +23,32 @@ if($_FILES['upload'] != null)
   // print_r($_FILES['upload']);
   // print_r($_FILES['upload']['error']);
 
-  if (move_uploaded_file($_FILES['upload']['tmp_name'], $uploadfile)) {
-    echo "<script>alert('성공');</script>";
-    echo "<script>location.href ='add.php';</script>";
-  } else {
-    echo "<script>alert('실패');</script>";
+  $type_list = $_POST['type'];
+  $type = '';
+  $idx = 0;
+  while($type_list[$idx] != null)
+  {
+    $type .= $type_list[$idx++]."_";
+  }
+  if($type == ''){$type = '없음';}
+  $sql = "INSERT INTO question_image (image,page, number,level,type_index_num,type)
+  VALUES ('".$file_name."', '".$_POST[page]."', '".$_POST[num]."', $_POST[difficult], $type_index_num[0] ,'".$type."')";
+  $result = mysqli_query($conn, $sql);
+  if($result != null)
+  {
+    echo "<script>alert('DB 등록 성공');</script>";
+  }else
+  {
+    echo "<script>alert('DB 등록 실패. 입력내용 확인 바람.');</script>";
   }
 
+  if (move_uploaded_file($_FILES['upload']['tmp_name'], $uploadfile)) {
+    echo "<script>alert('사진 등록 성공');</script>";
+    echo "<script>location.href ='add.php';</script>";
+  } else {
+    echo "<script>alert('사진 등록 실패');</script>";
+    echo "<script>location.href ='add.php';</script>";
+  }
 
 }
  ?>
