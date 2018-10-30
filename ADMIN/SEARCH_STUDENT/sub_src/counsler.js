c77b84902cae2fd14de171dbf1fd2906099994d2
Eumhongin <zizon0wpxm@naver.com>
2018-10-30 16:59:11 +0900

    수정완료

--- ADMIN/SEARCH_STUDENT/sub_src/counsler.js
+++ ADMIN/SEARCH_STUDENT/sub_src/counsler.js
@@ -58,9 +58,8 @@ $('.writing > p').on('click',function(){
       'margin-top':($('.writing').outerHeight()-$('span').outerHeight())/2+'px'
     });
     //수정버튼을 누를때.
-<<<<<<< HEAD
     var tempdata = $('textarea').val();
-    $('.modify_btn').on('click',function(){ 
+    $('.modify_btn').on('click',function(){
       if($(this).hasClass('Btn_Actived')){
         //
         let r = confirm("수정 하시겠습니까?");
@@ -78,10 +77,6 @@ $('.writing > p').on('click',function(){
      else{
        click_modify(box,tempdata);
      }
-=======
-    $('.modify_btn').on('click',function(){
-      click_modify(box,$('textarea').val());
->>>>>>> 2804698ea408119f97da51bc7f51f994fccb2839
     });
   }
   else{
@@ -106,19 +101,6 @@ function free_modify(){
   $('.modify_textarea').remove();
 
 }
-
-function click_modify(box,text){
-
-  $('.recent_view').remove();
-  $('<textarea class="modify_textarea">').css({
-    'width':'100%',
-    'height':$(box).outerHeight()*0.6+'px',
-    'border':'none'
-  }).appendTo(box);
-  $('.modify_textarea').val(text);
-};
-
-
 
 function getContents()
 {
@@ -163,9 +145,10 @@ function setContents(set)
 {
   $.ajax({
     url:"sub_src/setAdviceContents.php",
+    dataType:"json",
     type:"POST",
     data :{set:set},
-    success:function()
+    success:function(result)
     {
       alert('완료');
     },
