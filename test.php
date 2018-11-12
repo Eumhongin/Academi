0d6365472277ae90a6d4a3dd3fdcda5b99ed73b9
Eumhongin <zizon0wpxm@naver.com>
2018-11-13 02:20:08 +0900

    1차 커밋

--- test.php
+++ test.php
@@ -1,30 +1,14 @@
 <!DOCTYPE html>
+<?php include('module/rand_string.php') ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
-    <script type="text/javascript">
-      if(confirm('진행함?'))
-      {
-        alert('ㅇㅇ');
-      }
-    </script>
   </head>
   <body>
     <?php
-    include("module\dbConnect.php");
-    include("module\dbContentsEcho.php");
-    include("module\EVENTdbConnect.php");
+    echo genRandom();
 
-    $get_num = "SELECT * FROM member WHERE stu_tea = 0";
-    $a = DB_echo($get_num, 'school_name');
-    $b = DB_echo($get_num, 'id');
-    $i = 0;
-
-    print_r($a);
-    echo "</br>";
-    print_r($b);
      ?>
-
   </body>
 </html>
