4dfeb0512418c6e9aff042af47a0acd71ece41e7
LimJoongSub <quhb2455@gmail.com>
2018-11-29 13:26:50 +0900

    18-11-29
    까먹고 몬올린 것.
    틀린 문제를 맞추면 리스트에서 아이디를 지워줌.

--- Teacher/marking.php
+++ Teacher/marking.php
@@ -11,6 +11,13 @@ include("..\module\url_access_denied.php");
 //         WHERE qi.no = '".$_POST[number][$idx]."'";
 
 $_SESSION['abcd'] = $_POST['number'];
+$idx3=0;
+
+$correctNum = DB_echo("SELECT question_num FROM question_list WHERE no = '".$_POST[no]."'", 'question_num');
+$correctNum = explode("|", $correctNum[0]);
+///이부분~
+
+
 
 for($idx = 0; $idx < count($_POST['number']); $idx++)
 {
@@ -25,7 +32,32 @@ for($idx = 0; $idx < count($_POST['number']); $idx++)
 
   $sql4 = "UPDATE question_image SET wrong_student = CONCAT(wrong_student,'|".$_SESSION[stu_id][0]."') WHERE no ='".$_POST[number][$idx]."'";
   mysqli_query($conn, $sql4);
+
+  for($idx2 = 0; $idx2 < count($correctNum); $idx2++)
+  {
+    if($_POST['number'][$idx] == $correctNum[$idx2])
+    {
+      $correctNum[$idx2] = null;///이부분~
+    }
+  }
 }
+
+
+for($idx2 = 0; $idx2 < count($correctNum); $idx2++)
+{
+  if($correctNum[$idx2] != null)
+  {
+    $sql = "SET SQL_SAFE_UPDATES=0";
+    mysqli_query($conn,$sql);
+
+    $sql = "UPDATE question_image SET wrong_student = REPLACE(wrong_student,'|".$_SESSION[stu_id][0]."', '') WHERE no = '".$correctNum[$idx2]."'";
+    mysqli_query($conn, $sql);
+
+    $sql = "SET SQL_SAFE_UPDATES=1";
+    mysqli_query($conn,$sql);
+  }
+}
+
 
 $sql3 = "UPDATE question_list SET checked=1  WHERE no = '".$_POST[no]."'";
 mysqli_query($conn, $sql3);
--- test.php
+++ test.php
@@ -1,14 +1,49 @@
 
 <?php
 session_start();
-// include("module\dbConnect.php");
-// include("module\dbContentsEcho.php");
+include("module\dbConnect.php");
+include("module\dbContentsEcho.php");
 
-$conn = mysqli_connect("localhost","root","misozium23");
-$sqli = mysqli_select_db($conn,"test");
 
-$time = date("Y-m-d H:i:s");
+// $conn = mysqli_connect("localhost","root","misozium23");
+// $sqli = mysqli_select_db($conn,"test");
 
-$sql = "UPDATE comment SET updatetime = '".$time."' WHERE todak = 5";
-mysqli_query($conn, $sql);
+$correctNum = DB_echo("SELECT question_num FROM question_list WHERE no = 5", 'question_num');
+
+$correctNum = explode("|",$correctNum[0]);
+print_r($correctNum);
+echo"</br>";echo"</br>";echo"</br>";
+$array = [37 ,87 ,51 ,23];
+
+for($idx = 0; $idx < count($array); $idx++)
+{
+  $sql4 = "UPDATE question_image SET wrong_student = CONCAT(wrong_student,'|".$_SESSION[stu_id][0]."') WHERE no ='".$array[$idx]."'";
+  mysqli_query($conn, $sql4);
+  for($idx2 = 0; $idx2 < count($correctNum); $idx2++)
+  {
+    if($array[$idx] == $correctNum[$idx2])
+    {
+      $correctNum[$idx2] = null;
+    }
+
+  }
+}
+print_r($correctNum);
+echo"</br>";echo"</br>";echo"</br>";
+
+for($idx2 = 0; $idx2 < count($correctNum); $idx2++)
+{
+  if($correctNum[$idx2] != null)
+  {
+    $sql = "SET SQL_SAFE_UPDATES=0";
+    mysqli_query($conn,$sql);
+
+    $sql = "UPDATE question_image SET wrong_student = REPLACE(wrong_student,'|".$_SESSION[stu_id][0]."', '') WHERE no = '".$correctNum[$idx2]."'";
+    mysqli_query($conn, $sql);
+
+    $sql = "SET SQL_SAFE_UPDATES=1";
+    mysqli_query($conn,$sql);
+  }
+}
+
        ?>
