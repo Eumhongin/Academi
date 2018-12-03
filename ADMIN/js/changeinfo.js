31d33446a356f976eaf28af15b9cc3baab168d6c
Eumhongin <zizon0wpxm@naver.com>
2018-12-03 14:35:33 +0900

    1

--- ADMIN/js/changeinfo.js
+++ ADMIN/js/changeinfo.js
@@ -1,10 +1,3 @@
 $('.wrapper').css({
   'grid':$(window).outerHeight()+'px / 50% 50%'
 });
-$('#selectedFile').on('change', function() {
-  $.each($('#selectedFile').prop("files"), function(k, v) {
-    var filename = v['name'];
-    $('.FilePath').text(filename);
-    // filename = "blahblah.jpg", without path
-  });
-});
